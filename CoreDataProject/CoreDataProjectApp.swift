13a0d2cad18b203d9d861d33a07d039fba2f5e73
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-31 18:12:53 +0200

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Vahtee Boo on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
